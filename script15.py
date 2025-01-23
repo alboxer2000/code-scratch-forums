ab5a1016754dfad99479bc29b90fede87b527e4c
alboxer2000 <20brigidialessandro@icxxsettembrerimini.edu.it>
2025-01-23 19:31:15 +0100

    Create script15.py

---
+++ script15.py
@@ -0,0 +1,5 @@
+list = ["Apple", "Banana", 1, 2, True, "Blue", "Scratch", 56, "Joshisaurio"]
+list.append("Follow me!")
+print(list)
+#Output:
+["Apple", "Banana", 1, 2, True, "Blue", "Scratch", 56, "Joshisaurio", "Follow me!"]
