3925f0c0d3dabe5eee11b8137ac8d3a28537e55a
alboxer2000 <20brigidialessandro@icxxsettembrerimini.edu.it>
2025-01-23 19:31:43 +0100

    Update script15.py

--- script15.py
+++ script15.py
@@ -1,5 +1,18 @@
+# script 1
 list = ["Apple", "Banana", 1, 2, True, "Blue", "Scratch", 56, "Joshisaurio"]
 list.append("Follow me!")
 print(list)
 #Output:
 ["Apple", "Banana", 1, 2, True, "Blue", "Scratch", 56, "Joshisaurio", "Follow me!"]
+
+# script 2
+class BlockStack:
+    def __init__(self, c):
+        self.blocks = []
+        self.shadows = []
+        self.c = c
+    
+    def json_constructor(self):
+        return self.blocks + self.shadows
+    def AddBlock(self, block):
+        self.blocks.append(block)
