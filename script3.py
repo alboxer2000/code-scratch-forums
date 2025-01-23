af2aad8cee541caf20c6113c065aeaa3fbb22039
alboxer2000 <20brigidialessandro@icxxsettembrerimini.edu.it>
2025-01-23 18:17:15 +0100

    Create script3.py

---
+++ script3.py
@@ -0,0 +1,9 @@
+Values = {}
+for i in range(0, 10): #The 10 is an example, you can use the amount of variables that you want
+   Values["x"+ str(i)] = "Value" #The "x" + str(i) adds them up, so if i = 2, it will make a key named x2
+#To access your dictionary's keys, you can do this:
+Values[x2] #This is an example
+#To view everything, type this in:
+Values
+#To add a value, do this:
+Values["KeyName"] = "Value"
